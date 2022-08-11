66afd1927830068eb2fdbd73367dab27af5c3f3e
Наталья Скороспелова <soltk7@yandex.ru>
2022-08-11 13:27:13 +0300

    Добавьте файлы проекта.

---
+++ GB_WA_HW1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GB_WA_HW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
